252eadbf26bde24b6f07d5cbb8f9bd914121757c
Sebastian Graf <sgraf1337@gmail.com>
2018-06-12 09:50:00 +0200

    Trying to unbreak creduce-master

--- overlays/creduce-master.nix
+++ overlays/creduce-master.nix
@@ -1,14 +1,17 @@
 self: super:
 
 {
-  creduce-master = super.creduce.overrideAttrs (old: {
-    name = "creduce-master";
+  creduce-master = (super.creduce.override {
+    inherit (super.llvmPackages_6) llvm clang-unwrapped;
+  }).overrideAttrs (old: {
     version = "2.8.0alpha";
+
+    buildInputs = old.buildInputs ++ [ super.perlPackages.SysCPU ];
 
     src = super.fetchFromGitHub {
       owner = "csmith-project";
       repo = "creduce";
-      rev = "de21e365e218d36caac5e05a220a26dca68ea920";
+      rev = "8e622ba74bc35c5a81299d3a34b9b14038d6a70";
       sha256 = "0dyfg1ibr9zybv0sqjasrjk90qsfz6rq2w9shjf58p8c6f5ss6js";
     };
   });

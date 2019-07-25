781c9c71fba7c6ed21546986fcd648f35fc202e4
Sebastian Graf <sgraf1337@gmail.com>
2019-07-25 10:56:33 +0000

    Use self.callPackage in overlays

--- overlays/lorri.nix
+++ overlays/lorri.nix
@@ -7,5 +7,5 @@ self: super: {
         rev = "8224dfb57e508ec87d38a4ce7b9ce27bbf7c2a81";
         sha256 = "0qk4lqgqx312v2knzjcj52bvf1kh4bxcpmbb8n45fg3hqviicbrl";
       });
-    in super.callPackage src { inherit src; };
+    in self.callPackage src { inherit src; };
 }

a654cb05c10d2a026b74a06eed933b51780c925d
Sebastian Graf <sgraf1337@gmail.com>
2019-10-02 09:31:54 +0000

    Stuff from MSRC I want to revert later

--- home/work.nix
+++ home/work.nix
@@ -3,35 +3,11 @@
 {
   imports = [ ./user-common.nix ];
 
-  home.packages = with pkgs; [
-    cabal-install
-    gcc_multi
-    ghc
-    gmp
-    jetbrains.idea-community
-    krb5Full
-    maven
-    # Mendeley pulls Qt and more stuff that needs to be compiled
-    # mendeley
-    ncurses
-    openjdk11
-    openldap
-    (openssh.override {
-      withKerberos = true;
-      withGssapiPatches = true;
-    })
-    sssd
-    # ycomp
-  ];
+  home.packages = with pkgs; [ ];
 
-  programs.git.userEmail = "sebastian.graf@kit.edu";
+  programs.git.userEmail = "sgraf1337@gmail.com";
 
   programs.zsh = {
-    shellAliases = {
-      # Can't use the git binary from nixpkgs :/
-      git = "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin git";
-      ssh = "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin ssh";
-    };
     localVariables = {
       USE_TMUX = "yes";
     };

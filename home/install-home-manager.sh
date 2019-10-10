7f76f90e8b8de31cbfbf156230f64bf461ae5e8a
Sebastian Graf <sgraf1337@gmail.com>
2019-10-10 13:45:30 +0200

    Remove unnecessary home-manager channel

--- home/install-home-manager.sh
+++ home/install-home-manager.sh
@@ -2,5 +2,4 @@
 
 # Install home-manager so that this config is picked up
 sudo mkdir -m 0755 -p /nix/var/nix/{profiles,gcroots}/per-user/$USER
-nix-channel --add https://github.com/rycee/home-manager/archive/release-$(cat "$(dirname $0)/release").tar.gz home-manager
 nix-shell https://github.com/rycee/home-manager/archive/release-$(cat "$(dirname $0)/release").tar.gz -A install

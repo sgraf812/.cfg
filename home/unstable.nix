ea4d82245d2ba8f4683002ebb8bc9c69c05835bc
Sebastian Graf <sgraf1337@gmail.com>
2020-02-23 12:57:53 +0100

    Bumping unstable

--- home/unstable.nix
+++ home/unstable.nix
@@ -1,5 +1,5 @@
 {
-  name = "unstable-19-11-19";
-  url = "https://github.com/NixOS/nixpkgs/archive/41d921292e922a6cd1aba64259341c244d4c2cc7.tar.gz";
-  sha256 = "01iq7phnmyz78qddxsjy6lnpgmzcffxk9h7k69sy61dbjsyy9b4q";
+  name = "unstable-23-02-20";
+  url = "https://github.com/NixOS/nixpkgs/archive/ea79a830dcf9c0059656da7f52835d2663d5c436.tar.gz";
+  sha256 = "0vqnfh99358v9ym5z9i3dsfy0l4xxgh9hr278pi1y11gdl092014";
 }

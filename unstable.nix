caa350297dd1c4eb6544d57f6ffcf34672d12884
Sebastian Graf <sgraf1337@gmail.com>
2020-05-07 15:44:41 +0200

    Bump unstable

--- unstable.nix
+++ unstable.nix
@@ -1,8 +1,8 @@
 {
-  name = "nixos-unstable-2020-03-29";
+  name = "nixos-unstable-2020-05-01";
   url = https://github.com/nixos/nixpkgs-channels/;
   ref = "refs/heads/nixos-unstable";
-  # Commit hash for nixos-unstable as of 2020-03-29
+  # Commit hash for nixos-unstable as of 2020-05-01
   # $(git ls-remote https://github.com/nixos/nixpkgs-channels nixos-unstable | cut -f)
-  rev = "ae6bdcc53584aaf20211ce1814bea97ece08a248";
+  rev = "fce7562cf46727fdaf801b232116bc9ce0512049";
 }

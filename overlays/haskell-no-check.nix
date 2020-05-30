70dae618840a56b5321a84380ca4634e29a1c3f0
Sebastian Graf <sgraf1337@gmail.com>
2020-05-30 13:50:10 +0200

    Revert "Don't check Haskell packages"
    This reverts commit 4b6a43e58142b0606507fe4c53622a68d54c357e.
    
    We'd need to recompile every Haskell package, because they aren't cached
    in Hydra. Yuck.

--- overlays/haskell-no-check.nix
+++
@@ -1,9 +0,0 @@
-self: super: {
-  haskell = super.haskell // {
-    packageOverrides = hself: hsuper: {
-      mkDerivation = args: hsuper.mkDerivation (args // {
-        doCheck = false;
-      });
-    };
-  };
-}

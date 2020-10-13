92003c2ae396ae6d67c9a360e9fcd7980a715670
Sebastian Graf <sgraf1337@gmail.com>
2020-10-13 22:46:54 +0200

    bump unstable because onedrive broke

--- unstable.nix
+++ unstable.nix
@@ -1,8 +1,8 @@
 {
-  name = "nixos-unstable-2020-07-24";
+  name = "nixos-unstable-2020-10-13";
   url = https://github.com/nixos/nixpkgs-channels/;
   ref = "refs/heads/nixos-unstable";
-  # Commit hash for nixos-unstable as of 2020-07-24
+  # Commit hash for nixos-unstable as of 2020-05-02
   # $(git ls-remote https://github.com/nixos/nixpkgs-channels nixos-unstable | cut -f1)
-  rev = "5717d9d2f7ca0662291910c52f1d7b95b568fec2";
+  rev = "84d74ae9c9cbed73274b8e4e00be14688ffc93fe";
 }

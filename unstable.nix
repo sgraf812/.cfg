4bfb2c05b84d718991cb7e6eacb76f4ae50d9c43
Sebastian Graf <sgraf1337@gmail.com>
2021-04-10 18:17:25 +0200

    Bump unstable

--- unstable.nix
+++ unstable.nix
@@ -1,8 +1,8 @@
 {
-  name = "nixos-unstable-2020-10-31";
+  name = "nixos-unstable-2021-04-05";
   url = https://github.com/nixos/nixpkgs/;
   ref = "refs/heads/nixos-unstable";
-  # Commit hash for nixos-unstable as of 2020-10-31
+  # Commit hash for nixos-unstable as of 2021-04-05
   # $(git ls-remote https://github.com/nixos/nixpkgs nixos-unstable | cut -f)
-  rev = "1dc37370c489b610f8b91d7fdd40633163ffbafd";
+  rev = "04a2b269d8921505a2969fc9ec25c1f517f2b307";
 }

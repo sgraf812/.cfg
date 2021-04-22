c22880a1851901146d43ae92a6749fdbf77ddb8c
Sebastian Graf <sgraf1337@gmail.com>
2021-04-22 21:18:40 +0200

    Add back common.nix
    A previous commit moved it from user-common.nix to common.nix, but I failed to
    track the new file.

---
+++ .config/nixpkgs/home/common.nix
@@ -0,0 +1,259 @@
+{ pkgs, lib, ... }:
+
+# Worth considering:
+# - [starship](https://starship.rs): Cool cross-shell prompt
+# - [sxhkd](https://github.com/baskerville/sxhkd): For X keyboard shortcuts
+# - muchsync: If I ever get the E-Mail stuff working
+# - xsuspend: Might be useful on the laptop
+# - getmail: Automatically fetch mail in a systemd service
+
+{
+  imports = [
+    modules/ghc-dev.nix
+    modules/cachix.nix
+    modules/kakoune.nix
+    modules/lazygit.nix
+    modules/tig.nix
+  ];
+
+  home.packages = with pkgs; [
+    audacity
+    bandwhich
+    bat
+    bench
+    binutils # ar and stuff
+    cabal2nix
+    cabal-install
+    cloc
+    creduce
+    dtrx
+    # dust # Needs pypy (WTF)
+    entr
+    exa
+    fd
+    fzf
+    ghc
+    glow # CLI markdown viewer
+    gnumake
+    # gthumb # can crop images # segfaults in ubuntu...
+    haskellPackages.ghcid
+    # haskellPackages.hkgr # Hackage release management, but it's broken
+    haskellPackages.lhs2tex
+    haskellPackages.hasktags
+    unstable.haskell-language-server
+    man
+    manpages
+    moreutils
+    ncdu
+    ncurses
+    niv
+    nix-diff
+    nix-index
+    nix-prefetch-scripts
+    nixpkgs-review
+    nofib-analyse # see overlay
+    p7zip
+    # parallel # GNU parallel + env_parallel, clashes with moreutils
+    sd
+    signal-desktop
+    stack
+    # stack2nix # broken
+    ranger
+    rename # prename -- https://stackoverflow.com/a/20657563/388010
+    ripgrep
+    tealdeer
+    tree
+    xclip # Maybe use clipit instead?
+    xdg_utils
+    vlc
+    ytop
+
+    # Haskell/Cabal/Stack stuff
+    # haskell-ci # old version, can't get it to work on unstable either
+    zlib.dev
+    gmp.static
+    numactl
+  ];
+
+  caches.cachix = [
+    "ghc-nix"
+  ];
+
+  programs.command-not-found.enable = true;
+
+  programs.zathura.enable = true;
+
+  programs.broot = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  # Used with lorri
+  programs.direnv = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Sebastian Graf";
+    aliases = {
+      a = "add";
+      ap = "add --patch";
+      abort = "rebase --abort";
+      amend = "commit --amend";
+      cat = "cat-file -p";
+      ci = "commit -a";
+      co = "checkout";
+      conflict = ''!"$EDITOR" -c '/^[<=|>]\\{7\\}' `git ls-files --unmerged | cut -c 51- | uniq`'';
+      contains = "branch --contains";
+      continue = "!git add -u && git rebase --continue";
+      cx = "commit";
+      da = "diff HEAD";
+      di = "diff";
+      dx = "diff --cached";
+      fixup = "commit --amend --reuse-message=HEAD"; # reuses timestamp and authorship info
+      # (f)etch (o)rigin and (s)witch to new branch from origin/master
+      fos = ''!bash -ec 'if (( $# != 1)); then echo >&2 git fos: 1 parameter expected; exit 2; fi; git fetch origin && git switch --create $1 --no-track origin/master' fos'';
+      graph = "log --decorate --graph";
+      less = "-p cat-file -p";
+      l = "log --decorate --graph --oneline";
+      lg = "log --decorate --graph --name-status";
+      s = "status -sb";
+      sf = "svn fetch";
+      suir = "submodule update --init --recursive";
+      tar = "archive --format=tar";
+      wta = "worktree add --detach"; # "worktree add --force --detach";
+      wtas = ''!bash -ec 'if (( $# != 1)); then echo >&2 git wtas: 1 parameter expected; exit 2; fi; tree=$(python -c "from __future__ import print_function; import os, os.path, sys; print(os.path.normpath(os.path.join(os.getenv(\"PWD\"), sys.argv[1])))" "$1"); git wta "$tree"; cd "$(git rev-parse --git-dir)"; for mod in $(git config --blob HEAD:.gitmodules -l --name-only|gawk -F . "/\\.path$/ {print \$2}"); do [ -d modules/$mod ] && git -C modules/$mod wta "$tree/$(git config --blob HEAD:.gitmodules --get submodule.$mod.path)"; done' wtas'';
+    };
+    extraConfig = {
+      core = {
+        editor = "kak";
+        pager = "less -x 4 -R -~"; # -F -c
+        # excludesfile = "$HOME/.gitignore";
+        whitespace = "trailing-space,space-before-tab";
+      };
+      color.ui = "auto";
+      push.default = "simple";
+      pull.ff = "only";
+      merge.conflictstyle = "diff3";
+    };
+  };
+
+  programs.home-manager.enable = true;
+
+  programs.vim = {
+    enable = true;
+    extraConfig = builtins.readFile vim/vimrc.vim;
+    settings = {
+      relativenumber = true;
+      number = true;
+    };
+    plugins = with pkgs.vimPlugins; [
+      ctrlp                # Fuzzy file finder etc.
+      nerdcommenter        # Comment scripts
+      nerdtree             # File browser
+      fugitive             # Git commands
+      sensible             # Sensible defaults
+      sleuth               # Heuristically set buffer options
+      # Solarized
+      airline              # Powerline in vimscript
+      vim-dispatch         # Asynchronous dispatcher
+      gitgutter            # Show git changes in gutter
+      # align              # Align stuff
+      tabular              # Also aligns stuff
+      tagbar               # ctags
+    ];
+  };
+
+  programs.zsh = {
+    enable = true;
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" ];
+      theme = "robbyrussell";
+    };
+    initExtra = builtins.readFile zsh/init.zsh;
+    sessionVariables = {
+      # disable default rprompt...?
+      RPROMPT = "";
+      # hide user in shell prompt
+      DEFAULT_USER = "sgraf";
+      # PAGER = "kak"; # doesn't understand color codes
+      EDITOR = "kak";
+      hardeningDisable = "fortify";
+    };
+    shellAliases = {
+      cfg = "git --git-dir=$HOME/.cfg/ --work-tree=$HOME";
+      lcfg = "${pkgs.unstable.lazygit}/bin/lazygit --git-dir=$HOME/.cfg/ --work-tree=$HOME";
+      nix-zsh = "nix-shell --command zsh";
+      nix-stray-roots = "nix-store --gc --print-roots | egrep -v '^(/nix/var|/run/\\w+-system|\\{memory)' | cut -d' ' -f1";
+      setclip = "xclip -selection clipboard -in";
+      getclip = "xclip -selection clipboard -out";
+      e = "kak";
+      less = ''\less -XFR'';
+      info = "info --vi-keys";
+      ls = "exa --color=automatic";
+      l = "ls -l";
+      ll = "l --group --header --links --extended --git";
+      la = "ll -a";
+      p = "(){ ${pkgs.python}/bin/python -c \"from math import *; print($*);\" }"; # https://stackoverflow.com/questions/34340575/zsh-alias-with-parameter#comment108551041_39395740
+    };
+  };
+
+  programs.tmux = {
+    enable = true;
+    extraConfig = builtins.readFile ./tmux/tmux.conf;
+    aggressiveResize = true;
+    baseIndex = 1;
+    keyMode = "vi";
+    shortcut = "a";
+    clock24 = true;
+    historyLimit = 50000;
+    terminal = "screen-256color"; # colors for kakoune etc.
+    secureSocket = false; # /run/user/$(id -u) is escaped in ZSH and won't work
+    plugins = with pkgs; [
+      tmuxPlugins.cpu
+      tmuxPlugins.resurrect
+      {
+        plugin = tmuxPlugins.continuum;
+        extraConfig = ''
+          set -g @continuum-restore 'on'
+        '';
+      }
+    ];
+  };
+
+  home.keyboard.layout = "de";
+
+  home.language = {
+    base = "en_US.UTF-8";
+    address = "de_DE.UTF-8";
+    monetary = "de_DE.UTF-8";
+    paper = "de_DE.UTF-8";
+    time = "de_DE.UTF-8";
+  };
+
+  home.stateVersion = "20.09";
+
+  services.lorri.enable = true;
+
+  systemd.user.services = {
+    onedrive = {
+      Unit = {
+        Description = "OneDrive Free Client";
+        Documentation = "man:onedrive(1)";
+        After = [ "local-fs.target" "network.target" ];
+      };
+
+      Service = {
+        ExecStart = "${pkgs.onedrive}/bin/onedrive --monitor";
+        Restart = "on-abnormal";
+      };
+
+      Install = {
+        WantedBy = [ "multi-user.target" ];
+      };
+    };
+  };
+}

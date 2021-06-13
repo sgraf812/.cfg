9cd5e34a5418dc412c1881c93e34e9d2bd3ea415
Sebastian Graf <sgraf1337@gmail.com>
2021-06-13 12:20:31 +0200

    Bump unstable

--- .config/nixpkgs/unstable.nix
+++ .config/nixpkgs/unstable.nix
@@ -1,8 +1,8 @@
 {
-  name = "nixos-unstable-2021-04-18";
+  name = "nixos-unstable-2021-06-13";
   url = https://github.com/nixos/nixpkgs/;
   ref = "refs/heads/nixos-unstable";
-  # Commit hash for nixos-unstable as of 2021-04-18
+  # Commit hash for nixos-unstable as of 2021-06-13
   # $(git ls-remote https://github.com/nixos/nixpkgs nixos-unstable | cut -f)
-  rev = "e5cc06a1e806070693add4f231060a62b962fc44";
+  rev = "432fc2d9a67f92e05438dff5fdc2b39d33f77997";
 }

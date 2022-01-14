55ebc72d85cd3703f4b960916af08e46fa1a4165
Sebastian Graf <sebastian.graf@kit.edu>
2022-01-14 19:58:48 +0100

    update unstable

--- .config/nixpkgs/unstable.nix
+++ .config/nixpkgs/unstable.nix
@@ -1,8 +1,8 @@
 {
-  name = "nixos-unstable-2021-12-02";
+  name = "nixos-unstable-2021-12-20";
   url = https://github.com/nixos/nixpkgs/;
   ref = "refs/heads/nixos-unstable";
-  # Commit hash for nixos-unstable as of 2021-12-02
+  # Commit hash for nixos-unstable as of 2021-12-20
   # $(git ls-remote https://github.com/nixos/nixpkgs nixos-unstable | cut -f)
-  rev = "6daa4a5c045d40e6eae60a3b6e427e8700f1c07f";
+  rev = "395879c28386e1abf20c7ecacd45880759548391";
 }

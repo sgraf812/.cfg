71c5b7a59d85176e31754661f7fb4b84e1998c77
Sebastian Graf <sgraf1337@gmail.com>
2022-01-29 19:09:26 +0100

    Delete old files

--- nixpkgs/unstable.nix
+++
@@ -1,8 +0,0 @@
-{
-  name = "nixos-unstable-2021-12-20";
-  url = https://github.com/nixos/nixpkgs/;
-  ref = "refs/heads/nixos-unstable";
-  # Commit hash for nixos-unstable as of 2021-12-20
-  # $(git ls-remote https://github.com/nixos/nixpkgs nixos-unstable | cut -f)
-  rev = "395879c28386e1abf20c7ecacd45880759548391";
-}
